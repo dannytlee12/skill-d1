b42d540099777567732297d9d2331a0e7f396e60
Danny Lee <dtl001@ucsd.edu>
2022-02-05 16:23:15 -0800

    put test in the right place

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -1,8 +1,9 @@
-import java.beans.Transient;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class SkillDemoTester {
+    @Test
     public void isOdd(){
-        @Test
         assertTrue(SkillDemo.isOdd(1));
     }
 }
